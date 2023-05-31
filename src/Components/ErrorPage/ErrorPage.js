aa1622c160187605b7720f7653eadcf25c5db6ed
zazimov <zalmanazimov@pursuit.org>
2023-05-31 19:08:59 -0400

    delete this

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -6,7 +6,7 @@ function ErrorPage() {
   return (
     <div>
       <div className="container mt-3 alert alert-dark w-50" role="alert">
-        No Results yet! Please submit a search above!
+        No Results yet! Please submit a search above!!!!!!
       </div>
       <button
         className="openModalBtn"
