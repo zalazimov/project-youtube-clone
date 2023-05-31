260a85952319d6ff519c920c5df577b7a62ff4c2
Jennifer <jenniferpeterson@pursuit.org>
2023-05-30 23:52:52 -0400

    few changes to About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,7 +6,11 @@ import "./About.css";
 function About() {
   return (
     <div className="container text-center lh-lg">
-      <h2 className="fw-bold mb-4">Welcome to our YouTube Clone Site!</h2>
+      <h2 className="fw-bold mb-4">Welcome to our YouTube Clone Site! </h2>
+      <img
+        className="logo"
+        src="https://www.freeiconspng.com/uploads/hd-youtube-logo-png-transparent-background-20.png"
+      />
       <div className="row">
         <p className="col-lg-4"></p>
         <p className="col-lg-4">
@@ -20,15 +24,20 @@ function About() {
         </p>
 
         <p className="text-center fs-5">Happy Watching!</p>
-        <p className="fs-3 p-5">A little bit about us:</p>
+        <h3 className="fs-3 p-5">About the Creators:</h3>
 
         <div className="image-container">
           <p className="col-5 mx-1">
-            <img src={zalman} alt="Zalman" className="image-style mb-1" />
-            <p className="fs-2 text-center">Zalman Azimov</p>A man of many hats,
-            I am a single father to a wonderful son, an avid explorer, a
-            problem-solver, and an aspiring full-stack developer with a passion
-            for good music, math, science, cooking, and a love for coding.
+            <img
+              src={zalman}
+              alt="Zalman"
+              className="image-style mb-1 zalman"
+            />
+            <p className="fs-2 text-center zalman">Zalman Azimov</p>A man of
+            many hats, I am a single father to a wonderful son, an avid
+            explorer, a problem-solver, and an aspiring full-stack developer
+            with a passion for good music, math, science, cooking, and a love
+            for coding.
             <p>
               {" "}
               <a href="#">https://github.com/zalazimov</a>
@@ -36,9 +45,9 @@ function About() {
           </p>
           <p className="col-5 mx-1">
             <img src={Jennifer} alt="Jen" className="image-style mb-1" />
-            <p className="text-center fs-2">Jennifer Peterson</p>An aspiring
-            Software Developer on my journey to learn as much as I possibly can
-            in order to help elevate black women and girls in tech.
+            <p className="text-center fs-2 jennifer">Jennifer Peterson</p>An
+            aspiring Software Developer on my journey to learn as much as I
+            possibly can in order to help elevate black women and girls in tech.
             <p>
               {" "}
               <a href="#">https://github.com/JenniferPeterson1203</a>
